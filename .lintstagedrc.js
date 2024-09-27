f99f976c28caabfa83bbdcdbdb48465fec39f211
gabriel <gabrieuvelezmoro@gmail.com>
2024-09-27 08:55:32 -0300

    Initial jest tests

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,6 +2,7 @@ module.exports = {
   //Lint & Prettify TS and JS files
   '*.{js,ts,jsx,tsx}': (filenames) => [
     `prettier --write ${filenames.join(' ')}`,
-    `npm run lint --fix . ${filenames.join(' --file')}`
+    `npm run lint --fix . ${filenames.join(' --file')}`,
+    `npm run test -- --findRelatedTests ${filenames.join(' ')}`
   ]
 }
