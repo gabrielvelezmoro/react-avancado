b6f15e780466336d2ef3fa252490f72aeae6ecb3
gabriel <gabrieuvelezmoro@gmail.com>
2024-11-02 12:10:12 -0300

    add plop

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,8 +1,20 @@
 import * as S from './styles'
 
-const Main = () => (
+const Main = ({
+  title = 'reacat avançado',
+  description = 'TypeScript, ReactJS, NextJS e Styled Components'
+}) => (
   <S.Wrapper>
-    <h1>React Avançado</h1>
+    <S.Logo
+      src="/public/img/logo.svg"
+      alt="Imagem de um átomo e React avançado ao lado"
+    />
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
+    <S.Illustration
+      src="/public/img/hero-illustration.svg"
+      alt="Um desenvolvedor de frente para uma tela com código"
+    />
   </S.Wrapper>
 )
 
